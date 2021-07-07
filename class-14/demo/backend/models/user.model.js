507d92bfe3ae89c877cfd53e739cd13c082c5ceb
yahyaalbashar <v.salvatore7.gs@gmail.com>
2021-07-07 15:22:47 +0300

    class 14

---
+++ class-14/demo/backend/models/user.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const catSchema = require('./cat.model');
+
+/*
+ We are going to use mongoose, to do two things:
+    - Create the schema
+    - generate the model
+*/
+
+// Here we are creating a new schema obj, which will be used later on to generate the model
+const userSchema = new mongoose.Schema({
+    email: { type: String },
+    cats: [catSchema]
+});
+
+// generate the model based on the schema
+const userModel = mongoose.model('users', userSchema);
+
+const seedUserData = () => {
+    const newUser = new userModel({
+        email: 'v.salvatore7.gs@gmail.com',
+        cats: [
+            { name: 'frankie' },
+            { name: 'leo' },
+            { name: 'oreo' }
+        ]
+    // }, {
+    //     email: 'v.salvatore7@gs.gmail.com',
+    //     cats: [
+    //         { name: 'Nimnim' },
+    //         { name: 'Booogie' },
+    //         { name: 'Namlah' }
+    //     ]
+    // 
+    });
+    console.log(newUser);
+    newUser.save();
+}
+
+module.exports = {
+    userModel,
+    seedUserData
+};
